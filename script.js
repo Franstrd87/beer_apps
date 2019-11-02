0559e54d77c4cd2c97362e704fca3b6275ece23e
Ever's PC <gomezsantiagoeverardo@gmail.com>
2019-11-02 10:42:29 -0700

    got the console log to return the correct information

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-$(document).on("click")
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://the-cocktail-db.p.rapidapi.com/filter.php?c=Cocktail",
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
-        "x-rapidapi-key": "85567027edmsh326c4363be56f0bp123f0bjsn555442865117"
+$(document).ready(function () {
+    function searchCocktail(userInput) {
+        let settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://the-cocktail-db.p.rapidapi.com/filter.php?c=Cocktail",
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+                "x-rapidapi-key": "85567027edmsh326c4363be56f0bp123f0bjsn555442865117"
+            }
+        }
+        $.ajax(settings).done(function (response) {
+            for (let i = 0; i < response.drinks.length; i++) {
+                if (response.drinks[i].strDrink.toLowerCase().includes(searchParameter.toLowerCase())) {
+                    console.log(response.drinks[i].idDrink)
+                    console.log(response.drinks[i].strDrink)
+                }
+                else {
+
+                }
+            }
+
+        })
     }
-}
-$.ajax(settings).done(function (response) {
-    console.log(response);
-});
+    $(document).on("click", "#")
+})
+
