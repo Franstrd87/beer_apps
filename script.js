8b7350a86370e3faa792c36b311cc1747cbb3205
Tulus Tiranda <tulustiranda@gmail.com>
2019-11-07 12:07:57 -0800

    new api added

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
-    function searchCocktail(searchParameter) {
+    let giphy = $(this).attr("id");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=GejTzVgcu0DoFlIQjwSnMjK4TX3eG3c3&q=cocktail&limit=4&offset=0&rating=G&lang=en";
+    console.log(giphy);
+    $("#giphy").empty()
+
+    $ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    });
+
+
+        function searchCocktail(searchParameter) {
         let settings = {
             "async": true,
             "crossDomain": true,
